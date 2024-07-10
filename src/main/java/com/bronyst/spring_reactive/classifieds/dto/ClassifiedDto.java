cfa886b31f5df4bcba5bfe70db7e6207a5f0ec05
nyadero <briannyadero443@gmail.com>
2024-07-10 15:43:33 +0300

    first commit

--- src/main/java/com/bronyst/spring_reactive/classifieds/dto/ClassifiedDto.java
+++
@@ -1,4 +0,0 @@
-package com.bronyst.spring_reactive.classifieds.dto;
-
-public class ClassifiedDto {
-}
--- src/main/java/com/bronyst/spring_reactive/classifieds/enums/ClassifiedCategory.java
+++
@@ -1,7 +0,0 @@
-package com.bronyst.spring_reactive.classifieds.enums;
-
-public enum ClassifiedCategory {
-    Vehicle,
-    Motorbike,
-    Bicycle
-}
